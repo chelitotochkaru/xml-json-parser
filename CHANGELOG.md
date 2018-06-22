e1bab4f403579a17886146bb92dc83037e05e7c2
Marcelo Gajardo <mgajardo@consensusgroup.net>
2018-06-22 01:15:21 -0300

    The transform() method now returns a Map<String, dynamic> object.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.3] - 06/22/2018
+
+* The transform() method now returns a Map<String, dynamic> object.
+
 ## [0.0.2] - 06/22/2018
 
 * The _convertNode() method return node.text always node isn't a XmlElement.
--- lib/xml_json_parser.dart
+++ lib/xml_json_parser.dart
@@ -1,32 +1,32 @@
 library xml_json_parser;
+
+import 'dart:convert';
+
 import 'package:xml/xml.dart' as Xml;
 
 class XmlJsonParser {
-  static dynamic _convertNode(node) {
+  static String _convertNode(node) {
     if (node is Xml.XmlElement) {
       if (node.children.length > 1) {
-        var child = [];
+        List<String> child = new List<String>();
         int counter = 0;
-        while(counter < node.children.length) {
-          final Xml.XmlNode currentNode = node.children[counter];
+        while (counter < node.children.length) {
+          Xml.XmlNode currentNode = node.children[counter];
           child.add(_convertNode(currentNode));
           counter++;
         }
-        return { node.name.toString(): child };
+        return '{"${node.name.toString()}": [${child.join(', ')}]}';
       } else {
-        return {
-          node.name.toString(): _convertNode(node.children[0])
-        };
+        return '{"${node.name.toString()}": ${_convertNode(node.children[0])}}';
       }
     }
-    return "'${node.text}'";
+    return '"${node.text}"';
   }
 
-  static List<dynamic> transform(String xmlString) {
-    //xml = xml.replaceAll(new RegExp(r'env:|ns1:|rpc:'), '');
-    final Xml.XmlDocument document = Xml.parse(xmlString);
-    final json = [];
-    json.add(_convertNode(document.children.where((node) => node is Xml.XmlElement).first));
-    return json;
+  static Map<String, dynamic> transform(String xmlString) {
+    //xmlString = xmlString.replaceAll(new RegExp(r'env:|ns1:|rpc:'), '');
+    Xml.XmlDocument document = Xml.parse(xmlString);
+    final String jsonString = _convertNode(document.children.where((node) => node is Xml.XmlElement).first);
+    return json.decode(jsonString);
   }
 }
